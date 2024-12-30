424b4038149453d344e88e4598124844b7eee41c
steve <steven.bergsman@gmail.com>
2024-12-30 08:20:34 -0600

    comps: 164 button start

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import Button from "./Button";
+
 function App() {
     return <div>
-        App
+        <div>
+            <Button>ClickMe</Button>
+        </div>
+        <div>
+            <Button>Steve</Button>
+        </div>
+        <div>
+            <Button>asd</Button>
+        </div>
+        <div>
+            <Button>dfgdfg</Button>
+        </div>
+        <div>
+            <Button>dfgdgfg</Button>
+        </div>
     </div>
 }
 
---
+++ src/Button.js
@@ -0,0 +1,7 @@
+import { Children } from "react";
+
+function Button({ children }) {
+    return <button>{children}</button>
+}
+
+export default Button;
