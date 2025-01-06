308afde366f8db7361a1592a4de5f3cd28d69c4f
steve <steve.bergsman@gmail.com>
2025-01-06 16:22:46 -0600

    292: comps

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,19 +1,34 @@
 
-import useCounter from "../hooks/use-counter";
+import { useState } from "react";
 import Button from "../components/Button";
+import Panel from "../components/Panel";
 
 
 
 function CounterPage({ initialCount }) {
     
-    const { count, increment } = useCounter(initialCount);
+    const [count, setCount] = useState(initialCount);
+    const increment = () => {
+        setCount(count + 1);
+    };
 
-    return <div>
-        <h1>Count is {count}</h1>
-        <Button onClick={increment}>
-            Increment
-        </Button>
-    </div>;
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <Panel className="m-3">
+            <h1 className="text-lg">Count is {count}</h1>
+            <div className="flex flex-row">
+                <Button onClick={increment}>
+                    Increment
+                </Button>
+                <Button onClick={decrement}>
+                    Decrement
+                </Button>
+            </div>
+        </Panel>
+    );
 };
 
 export default CounterPage;
